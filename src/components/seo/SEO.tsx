ac7642d7e64f8d254b0bba375eaa09e49ae830fb
sptungG <ngoviettung154@gmail.com>
2023-10-01 19:18:59 +0700

    update

--- src/components/seo/SEO.tsx
+++ src/components/seo/SEO.tsx
@@ -24,7 +24,7 @@ const SEO = ({ title, description, keywords }: TSEOProps) => {
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
+          content="width=device-width, maximum-scale=1.0, initial-scale=1.0, user-scalable=no"
         />
         <meta name="description" content={metaDescription} />
         <meta name="keywords" content={metaKeywords} />
